fe0205bf0ea1033ca9324f2419e13a2c94b2569d
GUANTAI001 <jpguantai@gmail.com>
2023-08-15 10:21:42 +0300

    fetched data from the api

---
+++ app/lib/getAllCharacters.ts
@@ -0,0 +1,16 @@
+//The function getAllCharacters() is an async function that fetches the data from the API 
+//and returns it as a JSON object. If the fetch fails, it throws an error.
+
+export default async function getCharacter() {
+    const apiURL = "https://hp-api.onrender.com/api/characters";
+    try {
+        const res = await fetch(apiURL);
+        if (!res.ok) {
+            throw new Error('Failed to fetch character');
+        }
+        return await res.json();
+    } catch (error) {
+        throw new Error('Failed to fetch or parse data');
+    }
+}
+
---
+++ app/lib/getCharacter.ts
@@ -0,0 +1,16 @@
+//the function that fetches data of a single character from the API.
+
+export default async function getCharacter(id: string) {
+    const apiURL = "https://hp-api.onrender.com/api/character/${id}";
+    try {
+        const res = await fetch(apiURL);
+        if (!res.ok) {
+            throw new Error('Failed to fetch character');
+        }
+        return await res.json();
+    } catch (error) {
+        throw new Error('Failed to fetch or parse data');
+    }
+}
+
+// Path: app/lib/getAllCharacters.ts
